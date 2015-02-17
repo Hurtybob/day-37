10211a4c13f187deac91f95ef97a47e0be7dad9f
Jesse Wolgamott <jesse@comalproductions.com>
2015-02-17 11:06:37 -0600

    Quiz answer and handlebars

--- movies/app/assets/javascripts/welcome.js
+++ movies/app/assets/javascripts/welcome.js
@@ -1,8 +1,31 @@
 $(window).ready(function(){
 
+  var jwo = {
+    firstName: "Jesse",
+    lastName: "Wolgamott",
+    fullName: function() {
+      return this.firstName + " " + this.lastName;
+    }
+  }
+
+  // alert(jwo.fullName());
+
+
   $.getJSON("/movies.json").then(function(data) {
 
-    console.log(data);
+    _.each(data.movies, function(movie){
+      var diff = movie.ratings.audience_score - movie.ratings.critics_score;
+      movie.ratings.differential = diff;
+    })
+
+    var sortedMovies = _.sortBy(data.movies, function(movie) { return movie.ratings.differential;})
+
+
+    var source   = $("#movie-template").html();
+    var template = Handlebars.compile(source);
+    var preparedHtml = template( sortedMovies );
+
+    $("#movies").html(preparedHtml);
 
   });
 
--- movies/app/controllers/welcome_controller.rb
+++ movies/app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def dashboard
+    @movies = []
   end
 end
